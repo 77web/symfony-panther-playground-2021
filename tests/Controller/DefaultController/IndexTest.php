5c99b1c4c8949a32fbd1b0382ca839c25f52858a
77web <info@77-web.com>
2021-12-25 16:29:36 +0900

    added test with Panther

--- tests/Controller/DefaultController/IndexTest.php
+++ tests/Controller/DefaultController/IndexTest.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 namespace App\Tests\Controller\DefaultController;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Panther\PantherTestCase;
 
-class IndexTest extends WebTestCase
+class IndexTest extends PantherTestCase
 {
     public function test()
     {
@@ -13,5 +14,22 @@ class IndexTest extends WebTestCase
         $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testPanther()
+    {
+        $client = static::createPantherClient();
+        $client->request('GET', '/');
+
+        $crawler = $client->waitForElementToContain('#type', 'search', 10);
+        $this->assertCount(4, $crawler->filter('#type option'), '1+3 options in types');
+        $this->assertSelectorIsDisabled('#vendor');
+
+        $crawler->filter('#type')->children()->eq(2)->click();
+        $client->takeScreenshot(__DIR__.'/../../../selectDisplayInType.png');
+        $crawler = $client->waitForElementToContain('#vendor', 'Googleディスプレイ広告', 10);
+        $this->assertCount(3, $crawler->filter('#vendor option'), '3 options in vendors');
+        $this->assertSelectorIsEnabled('#vendor');
+        $this->assertSelectorAttributeContains('#vendor option', 'value', 'gdn');
     }
 }
